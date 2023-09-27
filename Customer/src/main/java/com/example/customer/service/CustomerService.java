97222c60bc7367d94403a94c3dc17b02656bad0e
Trong-Nguyen-DT <103616323+Trong-Nguyen-DT@users.noreply.github.com>
2023-09-27 14:48:45 +0700

    mana Order Customer done

--- Customer/src/main/java/com/example/customer/service/CustomerService.java
+++ Customer/src/main/java/com/example/customer/service/CustomerService.java
@@ -1,8 +1,13 @@
 package com.example.customer.service;
 
 import com.example.customer.domain.Customer;
+import com.example.customer.domain.Order;
 import com.example.customer.entity.CustomerEntity;
+import com.example.customer.entity.OrderDetailHistoryEntity;
 import com.example.customer.entity.OrderEntity;
+import com.example.customer.entity.OrderHistoryEntity;
+
+import java.util.List;
 
 public interface CustomerService {
 
@@ -11,4 +16,8 @@ public interface CustomerService {
     Customer getCustomerByPhone(String phoneNumber);
 
     void setPoint(OrderEntity orderEntity);
+
+    List<OrderHistoryEntity> getAllOrderByCustomerId(Long id);
+
+    List<OrderDetailHistoryEntity> getAllOrderDetailByOrderId(Long id);
 }
